7ae81c30cbd3e4004d3d41fc68834e7d4a19e12a
David Zeindler <david.zeindler@bluewin.ch>
2024-03-26 20:32:55 +0000

    database connected

--- backend/controller/tasksController.js
+++ backend/controller/tasksController.js
@@ -1,21 +1,23 @@
-const getTasks = (req,res) => {
-    res.status(200).json({message: 'Get All Tasks'});
-}
+const asyncHandler = require('express-async-handler')
 
-const setTask =(req, res) => {
+const getTasks = asyncHandler(async(req,res) => {
+    res.status(200).json({message: 'Get All Tasks'});
+})
+
+const setTask = asyncHandler(async(req, res) => {
     if(!req.body.text){
         res.status(400);
         throw  new Error('Please enter a Task');
     }
     res.status(200).json({message: 'Created Task'});
-}
+})
 
-const updateTask =(req, res) => {
+const updateTask = asyncHandler (async(req, res) => {
     res.status(200).json({message: `Updated Task ${req.params.id}`});
-}
+})
 
-const deleteTask = (req, res) => {
+const deleteTask = asyncHandler (async (req, res) => {
     res.status(200).json({message: `Deleted Task ${req.params.id}`});
-}
+})
 
 module.exports = { getTasks, setTask, updateTask, deleteTask }

16c3319946976c53847a1f625f609f0243beb5fa
David Zeindler <david.zeindler@bluewin.ch>
2024-03-30 18:02:21 +0000

    add Header with login & register Link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from '../src/app/components/Dashboard';
 import Login from '../src/app/components/Login';
 import Register from '../src/app/components/Register';
+import Header from '../src/app/components/header';
 
 
 function App() {
   return (
-    <Router>
+    
+    <Router>     
       <div className='container' >
+        <Header />
         <Routes>
           <Route path='/' element={<Dashboard/>} />
           <Route path='/login' element={<Login/>} />
--- frontend/src/app/components/Dashboard.js
+++ frontend/src/app/components/Dashboard.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from './header'
 
 const Dashboard = () => {
     return (
-        <div>Dasboard</div>
+        <>
+        <div>
+            Dasboard
+        </div>
+        </>
     )
 }
 

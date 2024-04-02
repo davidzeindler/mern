440f8bd795c759c48ee6cf83b075d3f6507165c0
David Zeindler <david.zeindler@bluewin.ch>
2024-04-02 18:36:59 +0000

    Creating Tasks works!

--- frontend/src/app/features/tasks/taskSlice.js
+++ frontend/src/app/features/tasks/taskSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import taskService from './taskService'
 
 const initialState = {
     tasks: [],
@@ -7,6 +8,22 @@ const initialState = {
     isLoading: false,
     message: ''
 }
+
+export const createTask = createAsyncThunk(
+  'tasks/create',
+  async (taskData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token
+      return await taskService.createTask(taskData, token)
+    } catch (error) {
+      const message =
+        (error.response && error.response.data && error.response.data.message) 
+        || error.message 
+        || error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
 
 export const taskSlice = createSlice({
     name: 'task',
